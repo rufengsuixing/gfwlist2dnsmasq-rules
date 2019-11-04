a40f933cdf2008049d8f60bfc0de748a496a50ad
rufengsuixing <1985151814@qq.com>
2019-11-04 23:23:35 +0800

    first

---
+++ gfw2dns.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+awk 'BEGIN{getline;}{
+s1=substr($0,1,1);
+if (s1=="!")
+{next;}
+if (s1=="@"){
+    $0=substr($0,3);
+    s1=substr($0,1,1);
+    white=1;}
+else{
+    white=0;
+}
+
+if (s1=="|")
+    {s2=substr($0,2,2);
+    if (s2=="|")
+        {
+        $0=substr($0,3);
+    }else{
+        split($0,d,"/");
+        $0=d[3];
+    }}
+else{
+split($0,d,"/");
+$0=d[1];
+}
+star=index($0,"*");
+if (star!=0)
+{
+    $0=substr($0,star+1);
+    s1=substr($0,1,1);
+}
+if (s1==".")
+{fin=substr($0,2);}
+else{fin=$0;}
+if (index($0,".")==0) next;
+match(fin,"^[0-9\.]+")
+if (RSTART==1 && RLENGTH==length(fin)) {print "ipset add gfwlist "fin>"/tmp/doipset.sh";next;}
+if (fin=="" || finl==fin) next;
+finl=fin;
+if (white==0)
+    {print("server=/"fin"/127.0.0.1#5335\nipset=/"fin"/gfwlist");}
+else{
+    print("server=/"fin"/#\nipset=/"fin"/whitelist");}
+}' /tmp/gfwlist.txt > /tmp/gfwlist.conf
---
+++ readme.md
@@ -0,0 +1,9 @@
+###脚本功能
+将 base64解过码的/tmp/gfwlist.txt 转换成dnsmasq的配置文件/tmp/gfwlist.conf，其中代理名单为address和ipset-gfwlist名单，address默认dns服务器为127.0.0.1:5335,白名单为address 和 ipset-whitelist名单，其中address为走dnsmasq默认dns的规则，另外会生成一个/tmp/doipset.sh，包含着gfwlist规则里的ip规则，格式为ipset add gfwlist [ip] <br>
+完整列表脚本在mt7621上的运行速度input 7974 output 7542+9
+```
+real    0m 1.54s
+user    0m 1.51s
+sys     0m 0.01s
+```
+注：为了效率并没有完整实现adblock的规则，但是已经覆盖到gfwlist所用的而且dnsmasq能够做到的规则
