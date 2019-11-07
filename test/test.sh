b2e4ab61c4d8a74dbb4f43bbe3fe1dc48641b0d5
rufengsuixing <1985151814@qq.com>
2019-11-07 08:54:37 +0800

    add debug test

---
+++ test/test.sh
@@ -0,0 +1,6 @@
+grep -E "\!|@|\|" /etc/dnsmasq.ssr/*
+grep "//" /etc/dnsmasq.ssr/*
+grep -E "http[s]*:" /etc/dnsmasq.ssr/*
+grep "*" /etc/dnsmasq.ssr/*
+grep -E "/[^\.]*/" /etc/dnsmasq.ssr/*
+grep -E "/[0-9\.]*/" /etc/dnsmasq.ssr/*
